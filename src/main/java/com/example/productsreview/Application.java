630adb6e7ab4f3f71913b30c08ab32676276ee55
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-15 11:41:18 -0300

    refactor: remove dotenv config

--- src/main/java/com/example/productsreview/Application.java
+++ src/main/java/com/example/productsreview/Application.java
@@ -1,6 +1,5 @@
 package com.example.productsreview;
 
-import com.example.productsreview.config.DotenvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        DotenvConfig.loadDotenv();
         SpringApplication.run(Application.class, args);
     }
 
