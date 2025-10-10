ad9685c7c013047f26bd5b4cb8e41863d6ad9bcd
Wesley00s <wesley300rodrigues@gmail.com>
2025-10-10 10:51:33 -0300

    feat: add token authentication filter

---
+++ src/main/java/com/example/productsreview/config/security/TokenAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package com.example.productsreview.config.security;
+
+import com.example.productsreview.listener.dto.AuthenticatedUser;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = recoverToken(request);
+
+        if (token != null && !token.isEmpty()) {
+            AuthenticatedUser user = tokenService.validateToken(token);
+
+            if (user != null) {
+                var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) {
+            return null;
+        }
+        return Arrays.stream(cookies)
+                .filter(c -> c.getName().equals("token"))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse(null);
+    }
+
+}
