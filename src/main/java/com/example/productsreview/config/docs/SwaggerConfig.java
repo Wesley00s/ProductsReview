16d6e2e3eb92e45f9837f168a61370cecb504b14
Wesley00s <wesley300rodrigues@gmail.com>
2025-10-10 10:51:28 -0300

    feat: add swagger configuration

---
+++ src/main/java/com/example/productsreview/config/docs/SwaggerConfig.java
@@ -0,0 +1,59 @@
+package com.example.productsreview.config.docs;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .servers(
+                        List.of(
+                                new Server().url("http://localhost:8081/reviews/api").description("Development Server"),
+                                new Server().url("https://api.reviews.com").description("Production Server")
+                        )
+                )
+                .info(new Info()
+                        .title("Ecommerce Products Reviews API")
+                        .description("Documentação geral da API do sistema de avaliações de produtos do E-commerce.")
+                        .contact(new Contact()
+                                .email("wesley300rodrigues@gmail.com")
+                                .name("Wesley Rodrigues")
+                                .url("https://github.com/Wesley00s/")
+                        )
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")
+                        )
+                );
+    }
+
+    @Bean
+    public GroupedOpenApi publicApiV1() {
+        return GroupedOpenApi.builder()
+                .group("v1") 
+                .pathsToMatch("/v1/**")
+                .addOpenApiCustomizer(openApi -> {
+                    openApi.setInfo(new Info()
+                            .title("Ecommerce Products Reviews API - V1")
+                            .version("1.0")
+                            .description("API versão 1.0, focada nos recursos iniciais do e-commerce. Esta versão é estável."));
+                })
+                .build();
+    }
+
+}
