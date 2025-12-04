2c6f3b042acde6d4ff184ecd6ce98eab798f7f6c
Wesley00s <wesley300rodrigues@gmail.com>
2025-12-04 11:36:04 -0300

    feat: add new CORS origins

--- src/main/java/com/example/productsreview/config/rest/CorsConfig.java
+++ src/main/java/com/example/productsreview/config/rest/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "http://192.168.10.162:4200", "http://localhost:8000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
                 .allowedHeaders("*")
                 .allowCredentials(true);
